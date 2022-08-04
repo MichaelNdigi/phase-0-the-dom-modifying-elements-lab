49ddef10a9b77b8796294f9e01fd331cb1f34fdf
Michael Ndigi <michael.ndigi@student.moringaschool.com>
2022-08-05 00:14:17 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const element = document.getElementById("main");  
+// deletes the element
+element.remove();
+
+let newHeader = document.createElement("h1");
+ newHeader.setAttribute("id","victory");
+
+newHeader.innerHTML = "YOUR - NAME is the champion";
